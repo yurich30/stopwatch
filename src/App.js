d6feff453ce5e2eb590912c1033235ce6da0305a
Yurii <yurich3098@gmail.com>
2021-03-11 15:24:12 +0200

    create stopwatch method

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { render } from "react-dom";
+import { interval, Subject } from "rxjs";
 
-
- 
 export default function App() {
 
-	const [time, setTime] = useState(0)
+	const [time, setTime] = useState(0);
+
+	useEffect(() => {
+		const stopwatch$ = new Subject();
+		interval(1000)
+		  .subscribe(() => {
+			setTime(val => val + 1000);
+		  });
+	},[])
 
 	return (
 		<div>
-			{time}
+			<h1>{ new Date(time).toISOString() }</h1>
 		</div>
 	);
 }
